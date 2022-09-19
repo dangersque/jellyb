f6aa626ff7084252ad8382cac5c70c8ca3bd7caa
dangersque <90795859+dangersque@users.noreply.github.com>
2022-09-19 12:04:55 -0500

    Add files via upload

---
+++ jellyb/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for jellyb project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jellyb.settings')
+
+application = get_asgi_application()
